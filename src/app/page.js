de2633ba084ae4e984803b5381bc960d0ad0a4e4
Dinesh Kumar <dineshvit11@gmail.com>
2025-04-09 23:11:16 +0530

    "false"

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 
 export default function Home() {
   return (
-    <div >
-    
+    <div>    
      <h1>Welcome to jobs portal</h1>
      <h1>Hello Sanjay</h1>
-     <h4>Samar</h4>
-     
-    
+     <h4>Samar</h4>    
     </div>
   );
 }
