2be1798bac1b830449c5cac774c86d2e1bad4d19
Dinesh Kumar <dineshvit11@gmail.com>
2025-04-22 22:41:31 +0530

    "remove three lines"

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,7 @@ export default function Home() {
     <div>    
      <h1>Welcome to jobs portal</h1>
      <h1>Hello Sanjay</h1>
-     <h4>Samar</h4>   
-     <h4>Samar</h4>  
-     <h4>hello done</h4> 
+  
     </div>
   );
 }
